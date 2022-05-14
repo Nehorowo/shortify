7a871b04615120a9e831750a27e634cfddce8053
Nehorowo <trianglek4nz@gmail.com>
2022-05-14 19:07:09 +0300

    Add copy link button

--- client/src/components/LinkList.jsx
+++ client/src/components/LinkList.jsx
@@ -4,6 +4,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy } from "@fortawesome/free-regular-svg-icons";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { Container, Grid } from "@mui/material";
 
 const LinkList = ({ links }) => {
   const [userLinks, setUserLinks] = React.useState([]);
@@ -12,7 +13,7 @@ const LinkList = ({ links }) => {
     setUserLinks(
       links.map((i, index) => {
         return { ...i, isCopied: false, id: index + 1 };
-      }),
+      })
     );
   }, [links]);
 
@@ -26,45 +27,50 @@ const LinkList = ({ links }) => {
     return <p className="center">No links exist</p>;
   }
   return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            <th>№</th>
-            <th>Original</th>
-            <th>Shortened</th>
-            <th>Open</th>
-            <th>Copy</th>
-          </tr>
-        </thead>
+    <Container maxWidth="md">
+      <Grid container spacing={3}>
+        <table>
+          <thead>
+            <tr>
+              <th>№</th>
+              <th>Original</th>
+              <th>Shortened</th>
+              <th>Open</th>
+              <th>Copy</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          {userLinks.map((link, index) => {
-            return (
-              <tr>
-                <td>{index + 1}</td>
-                <td>{link.from}</td>
-                <CopyToClipboard text={link.to}>
-                  <td>{link.to}</td>
-                </CopyToClipboard>
-                <td>
-                  <Link to={`/detail/${link._id}`}>Open details</Link>
-                </td>
-                <td>
-                  {link.isCopied === false ? (
-                    <CopyToClipboard text={link.to} onCopy={() => copyLink(index)}>
-                      <FontAwesomeIcon icon={faCopy} />
-                    </CopyToClipboard>
-                  ) : (
-                    <FontAwesomeIcon icon={faCheck} />
-                  )}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
+          <tbody>
+            {userLinks.map((link, index) => {
+              return (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{link.from}</td>
+                  <CopyToClipboard text={link.to}>
+                    <td>{link.to}</td>
+                  </CopyToClipboard>
+                  <td>
+                    <Link to={`/detail/${link._id}`}>Open details</Link>
+                  </td>
+                  <td>
+                    {link.isCopied === false ? (
+                      <CopyToClipboard
+                        text={link.to}
+                        onCopy={() => copyLink(index)}
+                      >
+                        <FontAwesomeIcon icon={faCopy} />
+                      </CopyToClipboard>
+                    ) : (
+                      <FontAwesomeIcon icon={faCheck} />
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </Grid>
+    </Container>
   );
 };
 
